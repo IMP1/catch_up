4d73ec68c935806aa36f8ec69552ab60af04dda5
IMP1 <IMP1@mail.com>
2019-09-03 15:26:14 +0100

    Add blank activities, fragments and views

---
+++ app/src/main/java/net/imp1/catchup/ContactEditActivity.kt
@@ -0,0 +1,12 @@
+package net.imp1.catchup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_edit)
+    }
+}
