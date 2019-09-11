eab913e1d60d9116504d6b1f973f254489d0c8c5
IMP1 <IMP1@mail.com>
2019-09-11 13:41:17 +0100

    Add time duration in list item

--- app/src/main/java/net/imp1/catchup/ContactListAdapter.kt
+++ app/src/main/java/net/imp1/catchup/ContactListAdapter.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
 
 class ContactListAdapter(context: Context, resource: Int) :
     ArrayAdapter<String>(context, resource) {
@@ -21,17 +24,48 @@ class ContactListAdapter(context: Context, resource: Int) :
         return values.size
     }
 
+    private fun getLastContactedTime(datetime: Date?) : String {
+        val now = Calendar.getInstance().timeInMillis
+        datetime?.let {
+            val duration = now - datetime.time
+            val days = TimeUnit.DAYS.convert(duration, TimeUnit.MILLISECONDS)
+            val weeks = days / 7
+            if (weeks > 0) {
+                return context.getString(R.string.weeks_ago, weeks)
+            }
+            if (days > 0) {
+                return context.getString(R.string.days_ago, days)
+            }
+            val hours = TimeUnit.HOURS.convert(duration, TimeUnit.MILLISECONDS)
+            if (hours > 0) {
+                return context.getString(R.string.hours_ago, hours)
+            }
+            val minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS)
+            if (minutes > 0) {
+                return context.getString(R.string.minutes_ago, minutes)
+            }
+            val seconds = TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS)
+            if (seconds > 10) {
+                return context.getString(R.string.seconds_ago, seconds)
+            }
+            return context.getString(R.string.just_now)
+        }
+        return context.getString(R.string.never)
+    }
+
     @Override
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater : LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val rowView : View = inflater.inflate(R.layout.contact_list_item, parent, false)
+        val contact = values[position]
+        val contactNameTextView : TextView = rowView.findViewById(R.id.contact_name)
+        val contactImageView : ImageView = rowView.findViewById(R.id.contact_image)
+        val contactTimeTextView : TextView = rowView.findViewById(R.id.last_contacted)
 
-        val textView : TextView = rowView.findViewById(R.id.label)
-        val imageView : ImageView = rowView.findViewById(R.id.icon)
-        val contactName = values[position]
-        textView.text = contactName
-        imageView.setImageResource(R.drawable.ic_launcher_foreground)
-        imageView.contentDescription = context.getString(R.string.contact_photo_description, contactName)
+        contactNameTextView.text = contact.name
+        contactTimeTextView.text = getLastContactedTime(contact.lastContacted)
+        contactImageView.setImageResource(R.drawable.ic_launcher_foreground)
+        contactImageView.contentDescription = context.getString(R.string.contact_photo_description, contact.name)
 
         return rowView
     }
