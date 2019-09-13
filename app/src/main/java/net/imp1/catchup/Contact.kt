01ff839b4aa72ea93a4cc2306ad169d665ad3bb9
IMP1 <IMP1@mail.com>
2019-09-13 15:09:54 +0100

    Change icon_uri to a photo bitmap

--- app/src/main/java/net/imp1/catchup/Contact.kt
+++ app/src/main/java/net/imp1/catchup/Contact.kt
@@ -1,12 +1,12 @@
 package net.imp1.catchup
 
-import android.net.Uri
+import android.graphics.Bitmap
 import java.util.*
 
 data class Contact (
     val id: Long,
     var name: String,
-    var icon_uri: Uri?,
+    var photo: Bitmap?,
     var lastContacted: Date?,
     var contactMethod: String?,
     var address: String?) {
